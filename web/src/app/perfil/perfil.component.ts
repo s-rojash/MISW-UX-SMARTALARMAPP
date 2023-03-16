363b3c792f18d206bcad883d8736ac1c3e9a3ddd
JohnAriasDev <ja.ariasa1@uniandes.edu.co>
2023-03-16 01:05:49 -0500

    fix perfil component

--- web/src/app/perfil/perfil.component.ts
+++ web/src/app/perfil/perfil.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.scss']
 })
 export class PerfilComponent {
+  constructor(private router: Router) {}
 
+  actualizar():void{
+    Swal.fire({
+      text: 'perfil actualizado Exitosamente.',
+      icon: 'success',
+      confirmButtonText: 'Aceptar',
+      customClass: {
+        confirmButton: 'ok-button-class'
+      }
+    }).then(() => {
+      this.router.navigateByUrl('/login');
+    });
+
+  }
 }
